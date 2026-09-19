7f9fb68ad4ab74daa46264bf629849eac7ab35e5
VIGNESH KUMAR M <vigneshvicky10072007@gmail.com>
2026-09-19 14:32:17 +0530

    Implement solution for reducing number to zero

---
+++ 1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int n=0;
+        while(num>0){
+            if(num%2==0) num=num/2;
+            else num--;
+            n++;
+        }
+        return n;
+    }
+};

2add8fa5a089047dd64ce60c8529ddceedd117df
VIGNESH KUMAR M <vigneshvicky10072007@gmail.com>
2026-09-25 14:32:11 +0530

    Implement countKDifference function to count pairs

---
+++ 2006. Count Number of Pairs With Absolute Difference K.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]-nums[j]==k || nums[j]-nums[i]==k) c++;
+            }
+        }
+        return c;
+    }
+};
